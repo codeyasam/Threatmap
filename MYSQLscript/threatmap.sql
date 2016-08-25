cff9dfa355001f279fa57b7f8202f9516ed9953c
Emmanuel Yasa <codeyasam@gmail.com>
2016-08-26 01:37:56 +0800

    [SQL - threatmap.sql] adds lat lng to client_tb

--- MYSQLscript/threatmap.sql
+++ MYSQLscript/threatmap.sql
@@ -36,6 +36,8 @@ CREATE TABLE CLIENT_TB (
 	middle_name VARCHAR(255) NOT NULL,
     display_picture VARCHAR(255),
 	address VARCHAR(255) NOT NULL,
+	lat FLOAT(10, 6) NOT NULL,
+	lng FLOAT(10, 6) NOT NULL,
 	contact_no VARCHAR(255) NOT NULL,
 	office_id INT(11),
 	CONSTRAINT client_office_id FOREIGN KEY(office_id)
@@ -65,16 +67,21 @@ VALUES ('Sumapang Matanda a Barangay Hall', 'Sumapa Ligas Rd, Malolos, Bulacan, 
 
 INSERT INTO END_USER_TB (first_name, last_name, middle_name, display_picture,
 address, contact_no, office_id, department, rank, username, password) VALUES (
-'Maryjo Estrella', 'Bautista', 'Delfin', 'DISPLAY_PICTURE/default_avatar.png', 'Sumapa Ligas Rd, Malolos, Bulacan, Philippines',
+'Maryjo Estrella', 'Bautista', 'Delfin', 'DISPLAY_PICTURES/default_avatar.png', 'Sumapa Ligas Rd, Malolos, Bulacan, Philippines',
 '09069081822', 1, 'Architecture Security', 'Architect', 'jojo', '7510d498f23f5815d3376ea7bad64e29');
+
+INSERT INTO END_USER_TB (first_name, last_name, middle_name, display_picture,
+address, contact_no, office_id, department, rank, username, password) VALUES (
+'Emmanuel', 'Pescasio', 'Yasa', 'DISPLAY_PICTURES/default_avatar.png', 'Sumapa Ligas Rd, Malolos, Bulacan, Philippines',
+'09069081822', 1, 'Software Architecture', 'Software Architect', 'yasa', '7510d498f23f5815d3376ea7bad64e29');
 
 #populate clients table
 INSERT INTO CLIENT_TB (first_name, last_name, middle_name, display_picture,
-address, contact_no, username, password, person_to_notify,
-relationship, identification_number) VALUES ('code', 'yasa', 'm', 'DISPLAY_PICTURE/default_avatar.png', '538 Daisy St, Malolos, 3000 Bulacan, Philippines', 
-'09194348867', 'codeyasam', '5ebe2294ecd0e0f08eab7690d2a6ee69', 'Amor Yasa', 'Mother', '1234567');
+address, lat, lng, contact_no, username, password, person_to_notify,
+relationship, identification_number) VALUES ('code', 'yasa', 'm', 'DISPLAY_PICTURES/default_avatar.png', '538 Daisy St, Malolos, 3000 Bulacan, Philippines', 
+'14.854902','120.835943','09194348867', 'codeyasam', '5ebe2294ecd0e0f08eab7690d2a6ee69', 'Amor Yasa', 'Mother', '1234567');
 
 INSERT INTO CLIENT_TB (first_name, last_name, middle_name, display_picture,
-address, contact_no, username, password, person_to_notify,
-relationship, identification_number) VALUES ('david', 'gasnerr', 'yeah', 'DISPLAY_PICTURE/default_avatar.png', '909 Sumapa Ligas Rd, Malolos, 3000 Bulacan, Philippines', 
-'09194348867', 'david', '5ebe2294ecd0e0f08eab7690d2a6ee69', 'Gasnerr Rin', 'Mother', '7654321');
+address, lat, lng, contact_no, username, password, person_to_notify,
+relationship, identification_number) VALUES ('david', 'gasnerr', 'yeah', 'DISPLAY_PICTURES/default_avatar.png', '909 Sumapa Ligas Rd, Malolos, 3000 Bulacan, Philippines', 
+'14.861677','120.844219','09194348867', 'david', '5ebe2294ecd0e0f08eab7690d2a6ee69', 'Gasnerr Rin', 'Mother', '7654321');
