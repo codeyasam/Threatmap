113a3be698f612b7070a8c6630d385742e401c6f
Emmanuel Yasa <codeyasam@gmail.com>
2016-08-26 01:45:40 +0800

    [PHP - user.php] fixes bug on authenticate and adds custom methods

--- includes/user.php
+++ includes/user.php
@@ -35,11 +35,49 @@
 			$sql .= "AND BINARY password = '{$password}' ";
 			$sql .= "LIMIT 1 ";
 
-			return $sql;
-			//$result_array = self::find_by_sql($sql);
-			//return !empty($result_array) ? array_shift($result_array) : false;
+			$result_array = self::find_by_sql($sql);
+			return !empty($result_array) ? array_shift($result_array) : false;
+		}
+		
+		public function display_name() {
+			if (isset($this->first_name) && isset($this->last_name)) {
+				return $this->first_name . " " . $this->last_name;
+			} else {
+				return "";
+			}
 		}
 
+		public function full_name() {
+			return $this->last_name . ", " . $this->first_name . " " . $this->middle_name;
+		}
 
+		//Override
+		public function getCustomFields() {
+			return array('id', 'display_picture','full_name', 'address', 
+			             'contact_no', 'office_id', 'department', 
+			             'rank', 'username', 'password');			
+		}
+
+		//Override
+		public function toJSON($customized=false) {
+			//if for display then customized it then if false do the old thing
+			if ($customized) {
+				$fValueArr = array();
+				foreach ($this->getCustomFields() as $key => $eachField) {
+					if ($eachField == "display_picture") {
+						$fValueArr[] = '"' . $eachField . '":"' . '<img src=\"' . htmlentities($this->$eachField) . '\"/>"';
+					} else if ($eachField == "full_name") {
+						$fValueArr[] = '"' . $eachField . '":"' . htmlentities($this->full_name()) . '"';	
+					} else if ($eachField == "office_id") {
+						$officeObj = Office::find_by_id($this->$eachField);
+						$fValueArr[] = '"' . $eachField . '":"' . htmlentities($officeObj->name) . '"';
+					} else {
+						$fValueArr[] = '"' . $eachField . '":"' . htmlentities($this->$eachField) . '"';		
+					}
+				}		
+				return join("," ,$fValueArr);		
+			}
+			return parent::toJSON();
+		}
 	}
 ?>
