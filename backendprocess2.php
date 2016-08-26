7d329a1752c0b451185571124a594f489ca531e1
Emmanuel Yasa <codeyasam@gmail.com>
2016-08-27 02:53:50 +0800

    [PHP - manageThreats.php] implements creat, delete, read of threats in map

---
+++ backendprocess2.php
@@ -0,0 +1,29 @@
+<?php require_once('includes/initialize.php'); ?>
+<?php 
+	//HANDLES UNATHORIZED ACCESS
+	$user = $session->is_logged_in() ? User::find_by_id($session->user_id) : false;
+	if (!$user) redirect_to('login.php');
+?>
+
+<?php 
+	//main processes
+	$output = "{";
+
+	if (isset($_POST['createThreat'])) {
+		$new_threat = new Threat();
+		$new_threat->address = trim($_POST['address']);
+		$new_threat->lat = trim($_POST['lat']);
+		$new_threat->lng = trim($_POST['lng']);
+		$new_threat->create();
+
+		$output .= '"newThreat":{' . $new_threat->toJSON() . '}';
+	} else if (isset($_GET['getThreats'])) {
+		$all_threats = Threat::find_all();
+		$output .= createJSONEntity("Threats", $all_threats);
+	} else if (isset($_POST['deleteThreat'])) {
+		Threat::delete_by_id($_POST['threat_id']);
+	}
+
+	$output .= "}";
+	echo $output;
+?>
--- manageThreats.php
+++ manageThreats.php
@@ -9,9 +9,161 @@
 	<head>
 		<title></title>
 		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<link rel="stylesheet" type="text/css" href="js/jquery-ui.css">
 	</head>
 	<body>
 		<?php getNavigation($user, true); ?>
+
+		<h3>MANAGE THREATS</h3>
+		<div class="actionBtnContainer">
+			<button title="Add Tool" id="addThreatBtn" type="button"></button>
+			<button title="Delete Tool" id="delThreatBtn" type="button"></button>
+			<button title="Drag Tool" id="dragThreatBtn" type="button"></button>
+			<!-- <button title="Select Tool" id="selectBranchBtn" type="button"></button> -->
+		</div>
+		<script src="https://maps.googleapis.com/maps/api/js?sensor=false&key=AIzaSyDDpPDWu9z820FMYyOVsAphuy0ryz4kt2o&libraries=places&sensor=false"></script>
 		
+		<!-- <div id="indexDetailContainer"></div> -->
+		<div id="map" class="main-window"></div>		
+		<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+		<script type="text/javascript" src="js/jquery-ui.min.js"></script>
+		<script type="text/javascript" src="js/functions.js"></script>
+		<script type="text/javascript" src="js/myGmapsInit.js"></script>
+		<script type="text/javascript">
+			markerOptions.animation = google.maps.Animation.DROP;
+			markerOptions.draggable = true;
+
+			var markers = [];
+			var selectedIndex = -1;
+			var toAdd = true;
+			var toDelete = false;
+			var toDrag = false;
+
+			processRequest("backendprocess2.php?getThreats=true");
+
+			function handleServerResponse() {
+				if (objReq.readyState == 4 && objReq.status == 200) {
+					console.log(objReq.responseText);
+					var jsonObj = JSON.parse(objReq.responseText);
+					
+					if (jsonObj.Threats) {
+						plotThreatsOnMap(jsonObj.Threats);
+					} else if (jsonObj.newThreat) {
+						console.log(markerOptions);
+						markerOptions.position = new google.maps.LatLng(jsonObj.newThreat.lat, jsonObj.newThreat.lng);
+						var marker = setMarkerValues(markerOptions, jsonObj.newThreat);
+						eventCallback(marker);
+					}
+				}
+			}
+
+			function plotThreatsOnMap(jsonThreats) {
+				for (var key in jsonThreats) {
+					if (jsonThreats.hasOwnProperty(key)) {
+						markerOptions.position = new google.maps.LatLng(jsonThreats[key].lat, jsonThreats[key].lng);
+						var marker = setMarkerValues(markerOptions, jsonThreats[key]);
+						eventCallback(marker);
+					}
+				}
+			}
+
+			$('#addThreatBtn').on('click', function() {
+				unSelectAllActionBtn();
+				toAdd = true;
+				toDelete = false;
+				toDrag = false;
+				console.log("add option clicked");
+			});
+
+			$('#delThreatBtn').on('click', function() {
+				unSelectAllActionBtn();
+				toDelete = true;
+				toAdd = false;
+				console.log("del option clicked");
+			});
+
+			$('dragThreatBtn').on('click', function() {
+				unSelectAllActionBtn();
+				toDelete = false;
+				toDrag = true;
+				toAdd = false;
+				console.log("drag option clicked");
+			});
+
+
+			function unSelectAllActionBtn() {
+
+			}
+
+			function getReverseGeocodingData(e, operation) {
+				var latLng = new google.maps.LatLng(e.latLng.lat(), e.latLng.lng());
+
+				var geocoder = new google.maps.Geocoder();
+				geocoder.geocode({'latLng': latLng}, function(results, status) {
+					if (status !== google.maps.GeocoderStatus.OK) {
+						alert("something went wrong. try again");
+					}
+					//checking to see if the geocode status is ok before proceeding
+					if (status == google.maps.GeocoderStatus.OK) {
+						var address = (results[0].formatted_address);
+
+						if (operation == "UPDATE") {
+
+						} else if (operation == "CREATE") {
+							processPOSTRequest("backendprocess2.php", "createThreat=true&address=" + address + "&lat=" + e.latLng.lat() + "&lng=" + e.latLng.lng());
+						}
+					}
+				});
+
+			}
+			//addMarker
+			google.maps.event.addListener(map, 'click', function(e) {
+				if (toDelete == false && toDrag == false) {	
+					console.log("map to add");
+					getReverseGeocodingData(e, "CREATE");
+				}
+			});
+
+			function setMarkerValues(markerOptions, jsonObject) {
+				var marker = new google.maps.Marker(markerOptions);
+				marker.setMap(map);
+				marker.id = jsonObject.id;
+				marker.address = jsonObject.address;
+				markers.push(marker);
+				console.log("set marker valeus");
+				return marker;
+			}
+
+			function deleteMarker(marker, markers) {
+				var threatID = marker.id;
+				processPOSTRequest("backendprocess2.php", "deleteThreat=true&threat_id=" + threatID);
+				var index = markers.indexOf(marker);
+				marker.setMap(null);
+				marker = null;
+				markers.splice(index, 1);
+			}
+
+			function selectMarker(marker, markers) {
+
+				selectedIndex = markers.indexOf(marker);
+			}
+
+			function eventCallback(marker) {
+				(function(marker) {
+					google.maps.event.addListener(marker, 'click', function() {
+						console.log("marker clicked " + toDelete);
+						if (toDelete == true) {
+							var action_performed = function() {
+								deleteMarker(marker, markers);
+								$('#dialog').dialog('close');
+							}
+							confirm_action("Are you sure you want to delete this threat?", action_performed);
+						}
+					})
+				})(marker);
+			}
+
+
+		</script>				
 	</body>	
 </html>
