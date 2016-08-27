95968d02f6fd8fb8ea1d7d463e0e639734ad261e
Emmanuel Yasa <codeyasam@gmail.com>
2016-08-27 19:03:25 +0800

    [PHP - office model] adds customization with the display of office model in table form

--- includes/office.php
+++ includes/office.php
@@ -5,14 +5,34 @@
 	class Office extends DatabaseObject {
 
 		protected static $table_name = "OFFICE_TB";
-		protected static $db_fields = array('id', 'name', 'address', 'contact_person', 'contact_no', 'lat', 'lng');
+		protected static $db_fields = array('id', 'name', 'address', 'municipality', 'contact_person', 'contact_no', 'lat', 'lng');
 
 		public $id;
 		public $name;
 		public $address;
+		public $municipality;
 		public $contact_person;
 		public $contact_no;
 		public $lat;
 		public $lng;
+
+		public static function getOfficeFields() {
+			return self::$db_fields;
+		}
+
+		public function getCustomFields() {
+			return array('id', 'name', 'address', 'contact_person', 'contact_no', 'lat', 'lng'); 
+		}
+
+		public function toJSON($customized=false) {
+			if ($customized) {
+				$fValueArr = array();
+				foreach ($this->getCustomFields() as $key => $eachField) {
+					$fValueArr[] = '"' . $eachField . '":"' . htmlentities($this->$eachField) . '"';		
+				}
+				return join(", ", $fValueArr);
+			}
+			return parent::toJSON();
+		}
 	}
 ?>
